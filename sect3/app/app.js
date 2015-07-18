f83ca80421987cf3ebbf2cc40c7924c13abdb523
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2015-07-18 15:02:44 -0700

    add examples of option1 2 3

--- sect3/app/app.js
+++ sect3/app/app.js
@@ -1,1 +1,10 @@
-var app = angular.module('customersApp', [ ]);
+
+// option 1
+// var app = angular.module('customersApp', [ ]);
+
+// option2
+(function(){
+
+  var app = angular.module('customersApp', []);
+
+}());
--- sect3/app/controllers/CustomersController.js
+++ sect3/app/controllers/CustomersController.js
@@ -1,6 +1,48 @@
 //option1
-app.controller('customersController', function($scope){
-      $scope.sortBy = 'name'; // default values for sorting by
+// app.controller('CustomersController', function($scope){
+//       $scope.sortBy = 'name'; // default values for sorting by
+//       $scope.reverse = false; //default values for sorting
+
+//       $scope.customers = [
+//       {joined:'2000-01-02', name:'John', city:'San Francisco', orderTotal:5.23432},
+//       {joined:'2000-02-02', name:'Larry', city:'Atlanta', orderTotal:7.23432},
+//       {joined:'2000-12-02', name:'Miguel', city:'New York City', orderTotal:8.23432},
+//       {joined:'2000-12-02', name:'Calvin', city:'San Francisco', orderTotal:8.23432},];
+//       $scope.doSort = function(propName){
+//         $scope.sortBy = propName;
+//         $scope.reverse = !$scope.reverse;
+//       };
+// });
+
+// option2
+
+// (function(){
+
+//   angular.module('customersApp')
+//   .controller('CustomersController', function($scope){
+//       $scope.sortBy = 'name'; // default values for sorting by
+//       $scope.reverse = false; //default values for sorting
+
+//       $scope.customers = [
+//       {joined:'2000-01-02', name:'John', city:'San Francisco', orderTotal:5.23432},
+//       {joined:'2000-02-02', name:'Larry', city:'Atlanta', orderTotal:7.23432},
+//       {joined:'2000-12-02', name:'Miguel', city:'New York City', orderTotal:8.23432},
+//       {joined:'2000-12-02', name:'Calvin', city:'San Francisco', orderTotal:8.23432},];
+//       $scope.doSort = function(propName){
+//         $scope.sortBy = propName;
+//         $scope.reverse = !$scope.reverse;
+//       };
+//   });
+
+// }())
+
+// option3
+
+
+(function(){
+
+  var CustomersController = function($scope){
+    $scope.sortBy = 'name'; // default values for sorting by
       $scope.reverse = false; //default values for sorting
 
       $scope.customers = [
@@ -12,5 +54,9 @@ app.controller('customersController', function($scope){
         $scope.sortBy = propName;
         $scope.reverse = !$scope.reverse;
       };
-})
+  };
 
+  angular.module('customersApp')
+  .controller('CustomersController', CustomersController);
+
+}())
