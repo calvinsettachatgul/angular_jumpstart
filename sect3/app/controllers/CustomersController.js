02fe581b644068ec71190346ef7e74eafda8c749
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2015-07-18 12:05:47 -0700

    Use with as syntax angular controller

--- sect3/app/controllers/CustomersController.js
+++ sect3/app/controllers/CustomersController.js
@@ -1,7 +1,7 @@
 //option1
-app.controller('CustomersController', function($scope){
-      $scope.sortBy = 'name';
-      $scope.reverse = false;
+app.controller('customersController', function($scope){
+      $scope.sortBy = 'name'; // default values for sorting by
+      $scope.reverse = false; //default values for sorting
 
       $scope.customers = [
       {joined:'2000-01-02', name:'John', city:'San Francisco', orderTotal:5.23432},
@@ -9,8 +9,7 @@ app.controller('CustomersController', function($scope){
       {joined:'2000-12-02', name:'Miguel', city:'New York City', orderTotal:8.23432},
       {joined:'2000-12-02', name:'Calvin', city:'San Francisco', orderTotal:8.23432},];
       $scope.doSort = function(propName){
-
-        $scope.sortBy= propName;
+        $scope.sortBy = propName;
         $scope.reverse = !$scope.reverse;
       };
 })
--- sect3/app/controllers/customersControllerWithAs.js
+++ sect3/app/controllers/customersControllerWithAs.js
@@ -1,16 +1,18 @@
-function CustomersController(){
+
+function customersController(){
       this.sortBy = 'name';
       this.reverse = false;
 
       this.customers = [
-      {joined:'2000-01-02', name:'John', city:'San Francisco', orderTotal:5.23432}, 
-      {joined:'2000-02-02', name:'Larry', city:'Atlanta', orderTotal:7.23432}, 
+      {joined:'2000-01-02', name:'John', city:'San Francisco', orderTotal:5.23432},
+      {joined:'2000-02-02', name:'Larry', city:'Atlanta', orderTotal:7.23432},
       {joined:'2000-12-02', name:'Miguel', city:'New York City', orderTotal:8.23432},
       {joined:'2000-12-02', name:'Calvin', city:'San Francisco', orderTotal:8.23432},];
-      debugger
+
       this.doSort = function(propName){
-        
         this.sortBy= propName;
         this.reverse=!this.reverse
       };
 }
+
+app.controller('customersController', customersController )
