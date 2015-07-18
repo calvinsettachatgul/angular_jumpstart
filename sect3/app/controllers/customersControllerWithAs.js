f3d4ffce1a926a95673699f2f6f58295fd9e223a
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2015-07-18 13:10:54 -0700

    Work out bugs in with as sytax

--- sect3/app/controllers/customersControllerWithAs.js
+++ sect3/app/controllers/customersControllerWithAs.js
@@ -8,11 +8,11 @@ function customersController(){
       {joined:'2000-02-02', name:'Larry', city:'Atlanta', orderTotal:7.23432},
       {joined:'2000-12-02', name:'Miguel', city:'New York City', orderTotal:8.23432},
       {joined:'2000-12-02', name:'Calvin', city:'San Francisco', orderTotal:8.23432},];
-
       this.doSort = function(propName){
-        this.sortBy= propName;
-        this.reverse=!this.reverse
+        this.sortBy = propName;
+        this.reverse = !this.reverse
       };
+
 }
 
 app.controller('customersController', customersController )
