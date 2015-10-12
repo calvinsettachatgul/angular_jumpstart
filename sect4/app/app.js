b7c0334b5dcabfbcc0a7f0bafbf97ecaf1a02c04
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2015-10-12 14:57:32 -0700

    Add routing

---
+++ sect4/app/app.js
@@ -0,0 +1,37 @@
+
+// option 1
+// var app = angular.module('customersApp', [ ]);
+
+// option2
+(function(){
+
+  var app = angular.module('customersApp', ['ngRoute']);
+
+  app.config(function($routeProvider){
+    //Routes go here
+
+    //givea path, controller, view
+
+    $routeProvider
+      .when('/',
+        {
+            controller: 'CustomersController',
+            templateUrl: 'app/views/customers.html'
+        })
+
+      .when('/orders',
+        {
+          controller: 'OrdersController',
+          templateUrl: '/app/views/orders.html'
+        })
+
+      .when('editCustomer/:customerId',
+        {
+          controller: 'CustomerEditController',
+          templateUrl: '/app/views/customerEdit.html'
+        });
+
+      .otherwise({ redirectTo: '/'});
+  });
+
+}());
