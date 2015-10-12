a361932d61d6937cb7e54064005de2806cdf2769
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2015-10-12 16:50:29 -0700

    Change notes.txt

--- sect3/app/controllers/CustomersController.js
+++ sect3/app/controllers/CustomersController.js
@@ -50,6 +50,7 @@
       {joined:'2000-02-02', name:'Larry', city:'Atlanta', orderTotal:7.23432},
       {joined:'2000-12-02', name:'Miguel', city:'New York City', orderTotal:8.23432},
       {joined:'2000-12-02', name:'Calvin', city:'San Francisco', orderTotal:8.23432},];
+
       $scope.doSort = function(propName){
         $scope.sortBy = propName;
         $scope.reverse = !$scope.reverse;
